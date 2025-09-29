24bc78532ef5fc8ea674e17439bf17627639cdb2
gsanchez <gabrielignaciosanchez99@gmail.com>
2025-09-29 10:25:03 -0300

    (feature) SWR localstorage provider

--- contexts/swr-context.jsx
+++ contexts/swr-context.jsx
@@ -3,10 +3,26 @@
 import { SWRConfig } from "swr";
 import { fetcher } from "@/lib/fetcher";
 
+function localStorageProvider() {
+  if (typeof window === "undefined") {
+    return new Map();
+  }
+
+  const map = new Map(JSON.parse(localStorage.getItem("app-cache") || "[]"));
+
+  window.addEventListener("beforeunload", () => {
+    const appCache = JSON.stringify(Array.from(map.entries()));
+    localStorage.setItem("app-cache", appCache);
+  });
+
+  return map;
+}
+
 export default function SWRProvider({ children }) {
   return (
     <SWRConfig
       value={{
+        provider: localStorageProvider,
         fetcher,
         revalidateOnFocus: true,
         revalidateOnReconnect: true,
